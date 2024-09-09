4c8e1c32e79b7c6c204de4fcfe6a48ce8f8620e0
Dwcks Dinh <corclan19@gmail.com>
2024-09-10 02:18:20 +0700

    fixed curl error (chocolatey)

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,11 +1,11 @@
 return {
-	"mistweaverco/kulala.nvim",
-	event = "VeryLazy",
-	config = function()
-		require("kulala").setup({
-                      default_view = "headers_body",
-                })
-
-		vim.api.nvim_set_keymap("n", "<C-\\>", ":lua require('kulala').run()<CR>", { noremap = true, silent = true })
-	end,
+  "mistweaverco/kulala.nvim",
+  opts = {
+    curl_path = "C:\\ProgramData\\chocolatey\\lib\\curl\\tools\\curl-8.9.1_1-win64-mingw\\bin\\curl.exe",
+    additional_curl_options = { "--insecure", "-A", "Mozilla/5.0" },
+    winbar = true,
+    default_winbar_panes = { "body", "headers", "headers_body" },
+    vscode_rest_client_environmentvars = true,
+    disable_script_print_output = true,
+  },
 }
