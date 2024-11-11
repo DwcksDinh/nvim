2eff81542da6ba798df16fe5c407f8f47e2995c5
Dwcks <dinhpdpd09993@fpt.edu.vn>
2024-11-11 14:54:23 +0700

    added some new keyword

--- init.lua
+++ init.lua
@@ -10,17 +10,17 @@ vim.opt.scrolloff = 8
 -- end
 
 -- Setting shell command flags
-vim.o.shellcmdflag =
-  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
-
--- Setting shell redirection
-vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
-
--- Setting shell pipe
-vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
-
--- Setting shell quote options
-vim.o.shellquote = "điếu thuốc tàn bên ly cà phê cạn"
+-- vim.o.shellcmdflag =
+--   "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+--
+-- -- Setting shell redirection
+-- vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+--
+-- -- Setting shell pipe
+-- vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
+--
+-- -- Setting shell quote options
+-- vim.o.shellquote = "điếu thuốc tàn bên ly cà phê cạn"
 
 -- vim.o.shell = 'pwsh'
 vim.o.shell = "nu"
