6facdfdeb878f54eeb74d535103ec61cc6b51ace
Dwcks <corclan19@gmail.com>
2025-05-07 16:29:36 +0700

    current

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,5 +1,6 @@
 return {
   "saghen/blink.cmp",
+  -- enabled = false,
   version = not vim.g.lazyvim_blink_main and "*",
   build = vim.g.lazyvim_blink_main and "cargo build --release",
   opts_extend = {
