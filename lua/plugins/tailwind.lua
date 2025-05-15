8d60d8baec274910fc71ab0ee73c70db5a894051
Dwcks <corclan19@gmail.com>
2025-05-15 10:14:59 +0700

    fix mason, tailwindfold and added note

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -1,11 +1,11 @@
 return {
-  "razak17/tailwind-fold.nvim",
-  opts = {},
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  event = "VeryLazy",
-  config = function()
-    require("tailwind-fold").setup({
-      ft = { "html", "svelte", "astro", "vue", "typescriptreact", "php", "blade", "htmlangular" },
-    })
-  end,
+  -- "razak17/tailwind-fold.nvim",
+  -- opts = {},
+  -- dependencies = { "nvim-treesitter/nvim-treesitter" },
+  -- event = "VeryLazy",
+  -- config = function()
+  --   require("tailwind-fold").setup({
+  --     ft = { "html", "svelte", "astro", "vue", "typescriptreact", "php", "blade", "htmlangular" },
+  --   })
+  -- end,
 }
